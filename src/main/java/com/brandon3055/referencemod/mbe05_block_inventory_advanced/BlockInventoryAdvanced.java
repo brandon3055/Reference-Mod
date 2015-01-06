13bbc524da8220c97c767db8fde3661c51314616
brandon3055 <brandon4261@gmail.com>
2015-01-07 08:59:34 +1100

    added MBE Inventory Examples

--- src/main/java/com/brandon3055/referencemod/mbe05_block_inventory_advanced/BlockInventoryAdvanced.java
+++ src/main/java/com/brandon3055/referencemod/mbe05_block_inventory_advanced/BlockInventoryAdvanced.java
@@ -6,7 +6,9 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
@@ -17,8 +19,10 @@ import net.minecraft.world.World;
  * User: brandon3055
  * Date: 06/01/2015
  *
- * BlockInventoryBasic is a simple inventory capable of storing 9 item stacks. The block itself doesn't actually store anything
- * the storage is handles by the associated tile entity. The block itself just creates said tile entity and opens the gui when right clicked.
+ * BlockInventoryAdvanced is an advanced furnace with 5 input, 4 output and 4 fuel slots that smelts at twice the speed
+ * of a regular furnace. The block itself doesn't do much more then any regular block except create a tile entity when
+ * placed, open a gui when right clicked and drop tne inventory's contents when harvested. Everything else is handled
+ * by the tile entity
  */
 public class BlockInventoryAdvanced extends BlockContainer
 {
@@ -42,5 +46,43 @@ public class BlockInventoryAdvanced extends BlockContainer
 		// Uses the gui handler registered to your mod to open the gui for the given gui id
 		playerIn.openGui(ReferenceMod.instance, GuiHandler.GUIID_EXAMPLE_ADVANCED, worldIn, pos.getX(), pos.getY(), pos.getZ());
 		return true;
+	}
+
+	// This is where you can do something when the block is broken. In this case drop the inventory's contents
+	@Override
+	public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+
+		IInventory inventory = worldIn.getTileEntity(pos) instanceof IInventory ? (IInventory)worldIn.getTileEntity(pos) : null;
+
+		if (inventory != null){
+			// For each slot in the inventory
+			for (int i = 0; i < inventory.getSizeInventory(); i++){
+				// If the slot is not empty
+				if (inventory.getStackInSlot(i) != null)
+				{
+					// Create a new entity item with the item stack in the slot
+					EntityItem item = new EntityItem(worldIn, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, inventory.getStackInSlot(i));
+
+					// Apply some random motion to the item
+					float multiplier = 0.1f;
+					float motionX = worldIn.rand.nextFloat() - 0.5f;
+					float motionY = worldIn.rand.nextFloat() - 0.5f;
+					float motionZ = worldIn.rand.nextFloat() - 0.5f;
+
+					item.motionX = motionX * multiplier;
+					item.motionY = motionY * multiplier;
+					item.motionZ = motionZ * multiplier;
+
+					// Spawn the item in the world
+					worldIn.spawnEntityInWorld(item);
+				}
+			}
+
+			// Clear the inventory so nothing else (such as another mod) can do anything with the items
+			inventory.clear();
+		}
+
+		// Super MUST be called last because it removes the tile entity
+		super.breakBlock(worldIn, pos, state);
 	}
 }
